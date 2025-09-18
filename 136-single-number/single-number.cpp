d6a67517c5ca7050e40c44e5f5c420cb68261b64
AakankshaPithala <142966273+AakankshaPithala@users.noreply.github.com>
2025-09-18 16:16:39 +0530

    Time: 353 ms (5.00%) | Memory: 20.7 MB (58.04%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -2,10 +2,16 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        int XOR = 0;
         for(int i=0;i<n;i++){
-            XOR = XOR ^ nums[i];
+            int num = nums[i];
+            int cnt =0;
+            for(int j =0;j<n;j++){
+                if(nums[j]==num){
+                    cnt++;
+                }
+            }
+            if(cnt==1) return num;
         }
-        return XOR;
+        return -1;
     }
 };

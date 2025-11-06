e544175cf2b75b6f2f4d76dec1951d9a4c36fb84
AakankshaPithala <142966273+AakankshaPithala@users.noreply.github.com>
2025-11-06 18:11:39 +0530

    Time: 3 ms (68.69%) | Memory: 14.8 MB (51.57%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,15 +1,42 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int,int> mpp;
-        for(int i=0;i<nums.size();i++){
-            int a = nums[i];
-            int more = target - nums[i];
-            if(mpp.find(more)!= mpp.end()){
-                return {mpp[more],i};
-            }
-            mpp[nums[i]] = i;
+        // map<int,int> mpp;
+        // for(int i=0;i<nums.size();i++){
+        //     int a = nums[i];
+        //     int more = target - nums[i];
+        //     if(mpp.find(more)!= mpp.end()){
+        //         return {mpp[more],i};
+        //     }
+        //     mpp[nums[i]] = i;
+        // }
+        // return {};
+         int n = nums.size();
+        vector<pair<int, int>> arr; // {value, original_index}
+        for (int i = 0; i < n; i++) {
+            arr.push_back({nums[i], i});
         }
-        return {};
+
+        // Sort by the values (first element of pair)
+        sort(arr.begin(), arr.end());
+
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+            int sum = arr[left].first + arr[right].first;
+
+            if (sum == target) {
+                return {arr[left].second, arr[right].second}; // ✅ original indices
+            } 
+            else if (sum > target) {
+                right--;
+            } 
+            else {
+                left++;
+            }
+        }
+
+        return {-1, -1};
     }
 };

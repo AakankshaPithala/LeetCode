7d9ba9aacd784531bc21c098f61e80b9249c8e71
AakankshaPithala <142966273+AakankshaPithala@users.noreply.github.com>
2025-11-18 17:54:50 +0530

    Time: 3 ms (54.92%) | Memory: 54.9 MB (97.75%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -2,12 +2,24 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
-        unordered_set<int> hash;
+        // unordered_set<int> hash;
+        // for(int i=0;i<n;i++){
+        //     hash.insert(nums[i]);
+        // }
+        // for(int i=1;i<=n;i++){
+        //     if(hash.find(i)==hash.end()) return i;
+        // }
+        // return n+1;
+
+        //moving the numbers to thier respective indexes
         for(int i=0;i<n;i++){
-            hash.insert(nums[i]);
+            while(nums[i]>=1 && nums[i]<=n && nums[i] != nums[nums[i] - 1]){
+                swap(nums[i],nums[nums[i]-1]);
+            }
         }
-        for(int i=1;i<=n;i++){
-            if(hash.find(i)==hash.end()) return i;
+        //check the first number where it is not eqaul to i+1;(we placed 3 at 2 index(following 0 indexed))
+        for(int i=0;i<n;i++){
+            if(nums[i]!= i+1) return i+1;
         }
         return n+1;
     }

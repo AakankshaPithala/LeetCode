74c5ce8b914d3a303db29f842e2c6fca2dbd86fb
AakankshaPithala <142966273+AakankshaPithala@users.noreply.github.com>
2025-11-20 17:41:52 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (81.33%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,17 +2,35 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int min = (n/3)+1;
-        vector<int> ls;
-        map<int,int> mpp;
+        int cnt1=0;int el1=0;
+        int cnt2 =0; int el2 =0;
+        //this for loop is only applying the moore's voting algorithm
         for(int i=0;i<n;i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it:mpp){
-            if(it.second > n/3){
-                ls.push_back(it.first);
+            if(el1==nums[i]) cnt1++;
+            else if(el2==nums[i]) cnt2++;
+            else if(cnt1==0 && nums[i]!=el2){
+                cnt1=1;
+                el1 = nums[i];
+            }
+            else if(cnt2==0 && nums[i]!=el1){
+                cnt2=1;
+                el2=nums[i];
+            }
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
+        cnt1=0;cnt2=0;
+        //manually check if its the majority element and push to the vector
+        vector<int> ls;
+        int mini= (n/3)+1;
+        for(int i=0;i<n;i++){
+            if(el1==nums[i])cnt1++;
+            if(el2==nums[i])cnt2++;
+        }
+        if(cnt1>=mini && el1!=el2) ls.push_back(el1);
+        if(cnt2>=mini ) ls.push_back(el2);
         return ls;
     }
 };

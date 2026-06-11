9615fc041b75318bfe45d1491dc07ed82a301ad7
AakankshaPithala <142966273+AakankshaPithala@users.noreply.github.com>
2026-06-11 17:24:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (79.12%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,27 +1,43 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string word ="";
-        stack<string> st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=' '){
-                word += s[i];
+        // string word ="";
+        // stack<string> st;
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]!=' '){
+        //         word += s[i];
+        //     }
+        //     else if(!word.empty()){
+        //         st.push(word);
+        //         word ="";
+        //     }
+        // }
+        // if(!word.empty()){
+        //     st.push(word);
+        // }
+        // string ans = "";
+        // while(!st.empty()){
+        //     ans += st.top();
+        //     st.pop();
+        //     if(!st.empty()){
+        //         ans += " ";
+        //     }
+        // }
+        // return ans;
+
+        string ans ="";
+        int i = s.size() - 1;
+        while(i>=0){
+            while(i>=0 && s[i]==' ') i-- ;
+            if(i<0) break;
+            int end=i;
+            while(i>=0 && s[i]!=' '){
+                i--;
             }
-            else if(!word.empty()){
-                st.push(word);
-                word ="";
-            }
-        }
-        if(!word.empty()){
-            st.push(word);
-        }
-        string ans = "";
-        while(!st.empty()){
-            ans += st.top();
-            st.pop();
-            if(!st.empty()){
+            if(!ans.empty()){
                 ans += " ";
             }
+            ans+= s.substr(i+1,end-i);//substr here acts like (start,length)
         }
         return ans;
     }
